d57662578acd9ae832b5f3ecf7e1c356fbf5347c
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-05-07 22:25:42 +0300

    Entry point for CPP-lib

---
+++ src/surface_geom/external_functions.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifdef RENGA_AUXILIARY_ACTIONS
+#define RENGA_AUXILIARY_ACTIONS_API __declspec(dllexport)
+#else
+#define RENGA_AUXILIARY_ACTIONS_API __declspec(dllimport)
+#endif
+#include <cstdint>
+extern "C" int32_t RENGA_AUXILIARY_ACTIONS_API __stdcall run_landxml2iges(const char* dir_path);
